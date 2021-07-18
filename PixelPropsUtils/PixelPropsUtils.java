b53dcca998c17161190acd5467227176e588b067
Sushrut Gupta <75667593+Sushrut1101@users.noreply.github.com>
2021-07-19 03:05:06 +0530

    evolution --> rr

--- PixelPropsUtils/PixelPropsUtils.java
+++ PixelPropsUtils/PixelPropsUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.internal.util.evolution;
+package com.android.internal.util.rr;
 
 import android.os.Build;
 import android.util.Log;
