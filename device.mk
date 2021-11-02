c20a554e3b9a3ad769a74aac482cc329b77f27ea
Sushrut1101 <guptasushrut@gmail.com>
2021-11-03 00:59:16 +0530

    A6020: Nuke a Deprecated Flag
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -46,7 +46,6 @@ PRODUCT_PACKAGES += \
     product_charger_res_images
 
 # Dex
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
